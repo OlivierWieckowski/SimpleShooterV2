8b3f4fc982e16202ca077c7fad1319dc8c7f0e14
OlivierWieckowski <95425040+OlivierWieckowski@users.noreply.github.com>
2024-05-08 23:23:30 +0200

    Setup6

---
+++ Source/SimpleShooterV2/SimpleShooterV2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleShooterV2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleShooterV2, "SimpleShooterV2" );
